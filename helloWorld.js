25b1aa0b77431fffa4c9d44c83079dde7d649ffc
Victor <otto85@gmail.com>
2019-10-06 17:28:43 +0300

    first routing

--- helloWorld.js
+++ helloWorld.js
@@ -1,9 +1,21 @@
 let http = require('http');
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-TYpe': 'text/plain'});
-    res.end('Hello, World');
-
+    let path = req.url;
+    switch(path) {
+        case '/':
+            res.writeHead(200, {'Content-TYpe': 'text/plain'});
+            res.end('HOMEPAGE');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-TYpe': 'text/plain'});
+            res.end('about');
+            break;
+        default:
+            res.writeHead(404, {'Content-TYpe': 'text/plain'});
+            res.end('Not found');
+            break;
+    }
 }).listen(3000);
 
 console.log('Сервер запущен н 3000');

70aeebd8905b8424d3e9547eb13f93c72e216c48
Victor Cherezov <victor_cherezov@mentor.com>
2019-10-07 12:49:56 +0300

    q

--- meadolwark/index.js
+++ meadolwark/index.js
@@ -1,18 +1,21 @@
 let express = require('express');
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+let fortune = require('./lib/forune.js')
 
 let app = express();
-let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(rea, res){
     res.render('home');
 });
 
 app.get('/about', function(rea, res){
-    res.render('about');
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 

8bb9ea1681ae42343279ea6746e8774913c2eed6
Victor Cherezov <victor_cherezov@mentor.com>
2019-10-07 11:46:50 +0300

    first handlebars

--- meadolwark/index.js
+++ meadolwark/index.js
@@ -1,33 +1,32 @@
 let express = require('express');
 
 let app = express();
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(rea, res){
-    res.type('text/plain');
-    res.send('Meadolwark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(rea, res){
-    res.type('text/plain');
-    res.send('About Meadolwark Travel');
+    res.render('about');
 });
-
 
 
 // 404 page
 app.use(function(req, res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - not found');
+    res.render('404');
 });
 
-// 505 page
+// 500 page
 app.use(function(err, req, res, next){
     console.log(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
